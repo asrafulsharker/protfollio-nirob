8b29b2e2b07f6e09fa25d88851f756e4e15e7bbb
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-05 23:14:41 +0600

    add servics

--- src/Services/service.js
+++ src/Services/service.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import 
+import React from 'react';
+import s1 from '../images/s-1.png';
+import s2 from '../images/s-2.png';
+import s3 from '../images/s-3.png';
+import s4 from '../images/s-4.png';
+import s5 from '../images/s-5.png';
+import s6 from '../images/s-6.png';
+
+
+import './service.css';
 function service() {
     return (
-        <div>
-            
+        <div style={{background:"#f5f7f7",padding:"100px"}}>
+            <div className="container">
+                <div className="row">
+                <div style={{margin:"39px",background:"white",padding:"30px"}} className="col-md-3 s-div">
+                   <img src={s1}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                <div style={{margin:"39px",background:"white",padding:"30px"}} className="col-md-3 s-div">
+                   <img src={s2}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                <div style={{margin:"39px",background:"white",padding:"30px"}} className="col-md-3 s-div">
+                   <img src={s3}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                <div style={{margin:"39px",background:"white",padding:"30px"}} className="col-md-3 s-div">
+                   <img src={s4}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                <div style={{margin:"39px",background:"white",padding:"30px"}} className="col-md-3 s-div">
+                   <img src={s5}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                <div style={{margin:"39px",background:"white",padding:"30px",cursor:"pointer"}} className="col-md-3 s-div">
+                   <img src={s6}/>
+                   <p style={{fontSize:"20px",fontWeight:"bold",marginTop:"15px"}}>Graphic Design</p>
+                   <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/home/home.js
+++ src/home/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../hero/hero';
 import About from '../about/about';
+import Service from '../Services/service';
 function home() {
     return (
         <div>
             <Hero/>
             <About/>
+            <Service/>
         </div>
     )
 }

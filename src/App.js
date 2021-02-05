c37bfa3fa1a65e2cbc251d603ce993c7fd006e9b
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-05 12:08:08 +0600

    add browser-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      
+    </>
   );
 }
 

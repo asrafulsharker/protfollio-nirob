80e2e782e60ee6fc551635d326e6a1ff12cbd820
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-06 10:26:25 +0600

    add contact form

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Img1 from '../images/background.jpg';
 function contact() {
     return (
-        <div>
+        <div className="container">
             <div className="row">
                 <div className="col-md-6">
                     <img src={Img1} style={{width:"100%"}}/>
@@ -12,13 +12,14 @@ function contact() {
                         <p>We give solution</p>
                         <p>If you have any questions
                         please don't hesitate to contact us</p>
-                        <form >
+                        <form  className="container">
                             <div className="row">
-                            <input type="text" placeholder="Name"/>
-                            <input type="text" placeholder="Name"/>
+                            <input className="col-md-6" type="text" style={{width:"80%",marginLeft:"20px",height:"50px"}} placeholder="Name"/>
+                            <input className="col-md-6"  style={{width:"80%",height:"50px",marginLeft:"20px"}} type="text" placeholder="Name"/>
+                            
+                            <input className="col-md-12"  style={{width:"500px",height:"50px",marginTop:"20px"}} type="text" placeholder="Name"/>
+                            <button type="submit">Submit</button>
                             </div>
-                            <input type="text" placeholder="Name"/>
-                            <button type="submit"/>
                         </form>
                     </div>
                 </div>
